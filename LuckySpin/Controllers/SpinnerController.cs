cb2713b2cc86e8fda3c11a179feff06db0e01536
Bryceells <bryceells@live.com>
2026-01-08 15:18:04 -0800

    Part 5: Spinner logic implemented with fixed number

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -12,6 +12,31 @@ namespace LuckySpin.Controllers
         public IActionResult Index(int luck) 
         {
             //TODO: add your string builder and HTML from Exercise 0 here
+        var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+Random random = new Random();
+
+
+
+app.MapGet("/", async (context) =>
+      {
+          int[] spin = new int[] { random.Next(10), random.Next(10), random.Next(10) };
+
+          System.Text.StringBuilder htmlToShow =  
+            new System.Text.StringBuilder("<body><h1>Lucky Spin - by Bryce H & Vadym R</h1><button onclick='history.go(0)'>Spin</button>");
+          htmlToShow.Append("<div>" + spin[0] + "</div>");
+          htmlToShow.Append("<div>" + spin[1] + "</div>");
+          htmlToShow.Append("<div>" + spin[2] + "</div>");
+
+          if (spin[0] == 7 || spin[1] == 7 || spin[2] == 7)
+          {
+            htmlToShow.Append("<img src='http://studentfolders.cascadia.edu/itweb285/LuckySpinCoins.jpg'/></body>");
+          }
+        
+                        
+          await context.Response.WriteAsync( htmlToShow.ToString() );
+       });
+
 
 
             //TODO: Modify this to use the string builder's response string as the Content property's value
