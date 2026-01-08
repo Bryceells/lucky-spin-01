a2dcf3490dc9b1940dae7c9faaf8a824e55b3711
Bryceells <bryceells@live.com>
2026-01-08 15:37:27 -0800

    Part 6: Variable changed from fixed to Luck int

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -13,25 +13,24 @@ namespace LuckySpin.Controllers
         {
             //TODO: add your string builder and HTML from Exercise 0 here
         var builder = WebApplication.CreateBuilder();
-var app = builder.Build();
-Random random = new Random();
+        var app = builder.Build();
+        Random random = new Random();
+        int Luck = random.Next(1, 10);
 
 
+        app.MapGet("/", async (context) =>
+            {
+                int[] spin = new int[] { random.Next(10), random.Next(10), random.Next(10) };
+                System.Text.StringBuilder htmlToShow =  
+                    new System.Text.StringBuilder("<body><h1>Lucky Spin - by Bryce H, Vadym R, & Alex Y</h1><button onclick='history.go(0)'>Spin</button>");
+                htmlToShow.Append("<div>" + spin[0] + "</div>");
+                htmlToShow.Append("<div>" + spin[1] + "</div>");
+                htmlToShow.Append("<div>" + spin[2] + "</div>");
 
-app.MapGet("/", async (context) =>
-      {
-          int[] spin = new int[] { random.Next(10), random.Next(10), random.Next(10) };
-
-          System.Text.StringBuilder htmlToShow =  
-            new System.Text.StringBuilder("<body><h1>Lucky Spin - by Bryce H & Vadym R</h1><button onclick='history.go(0)'>Spin</button>");
-          htmlToShow.Append("<div>" + spin[0] + "</div>");
-          htmlToShow.Append("<div>" + spin[1] + "</div>");
-          htmlToShow.Append("<div>" + spin[2] + "</div>");
-
-          if (spin[0] == 7 || spin[1] == 7 || spin[2] == 7)
-          {
-            htmlToShow.Append("<img src='http://studentfolders.cascadia.edu/itweb285/LuckySpinCoins.jpg'/></body>");
-          }
+                if (spin[0] == Luck || spin[1] == Luck || spin[2] == Luck)
+                {
+                    htmlToShow.Append("<img src='http://studentfolders.cascadia.edu/itweb285/LuckySpinCoins.jpg'/></body>");
+                }
         
                         
           await context.Response.WriteAsync( htmlToShow.ToString() );
